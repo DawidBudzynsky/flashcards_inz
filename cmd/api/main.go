eed2143a7496b8f13f5cadc4fbe6bbaa3f6bfbb3
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-11-27 15:09:16 +0100

    add auth to main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flashcards/internal/auth"
 	"flashcards/internal/server"
 	"fmt"
 )
 
 func main() {
+	auth.NewAuth()
 	server := server.NewServer()
 
 	err := server.ListenAndServe()
