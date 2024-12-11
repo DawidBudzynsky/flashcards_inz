d6de2261206ecbcc9ce28bd64f0bc30016151510
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-12-11 14:40:04 +0100

    middleware that uses context to get user id

--- internal/middlewares/checkLoggedIn.go
+++ internal/middlewares/checkLoggedIn.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"context"
 	"flashcards/internal/util"
 	"net/http"
 )
@@ -14,5 +15,23 @@ func CheckLoggedInMiddleware(next http.Handler) http.Handler {
 			return
 		}
 		next.ServeHTTP(w, r)
+	})
+}
+
+type ContextKey string
+
+const UserIDKey ContextKey = "user_id"
+
+// middleware
+func UserIdFromSession(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userId, err := util.GetUserIDFromSession(r)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserIDKey, userId)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
