b364021e86099aa6caae1ad8370ddab671fbd9ff
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-12-11 14:40:29 +0100

    update models

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,20 +3,20 @@ package models
 import "time"
 
 type User struct {
-	ID             int            `gorm:"primaryKey"`
-	GoogleID       string         `gorm:"size:255"`
-	Username       string         `gorm:"size:255"`
+	// ID             int            `gorm:"primaryKey"`
+	GoogleID       string         `gorm:"primaryKey" json:"google_id"`
+	Username       string         `gorm:"size:255;not null"`
 	Email          string         `gorm:"size:255;unique"`
 	Role           string         `gorm:"size:255"`
 	CreatedAt      time.Time      `gorm:"autoCreateTime"`
-	FlashcardsSets FlashcardsSets `gorm:"foreignKey:UserID"`
-	Folders        Folders        `gorm:"foreignKey:UserID"`
+	FlashcardsSets FlashcardsSets `gorm:"foreignKey:UserGoogleID"`
+	Folders        Folders        `gorm:"foreignKey:UserGoogleID"`
 }
 type Users []User
 
 type Folder struct {
 	ID             int            `gorm:"primaryKey"`
-	UserID         int            `gorm:"index"` // Foreign key
+	UserGoogleID   string         `gorm:"index"` // Foreign key
 	Name           string         `gorm:"size:255"`
 	Description    string         `gorm:"size:255"`
 	CreatedAt      time.Time      `gorm:"autoCreateTime"`
@@ -26,19 +26,19 @@ type Folder struct {
 type Folders []Folder
 
 type FlashcardSet struct {
-	ID          int       `gorm:"primaryKey"`
-	UserID      int       `gorm:"index"` // Foreign key
-	Title       string    `gorm:"size:255"`
-	Description string    `gorm:"size:255"`
-	CreatedAt   time.Time `gorm:"autoCreateTime"`
-	FolderID    int       `gorm:"index"`
-	Flashcards  Flashcards
+	ID           int       `gorm:"primaryKey"`
+	UserGoogleID string    `gorm:"index"` // Foreign key
+	Title        string    `gorm:"size:255"`
+	Description  string    `gorm:"size:255"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	FolderID     int       `gorm:"index"`
+	Flashcards   Flashcards
 }
 type FlashcardsSets []FlashcardSet
 
 type Flashcard struct {
 	ID             int       `gorm:"primaryKey"`
-	FlashcardSetID int       `gorm:"index"`
+	FlashcardSetID int       `gorm:"index"` // Foreign key
 	Question       string    `gorm:"size:255"`
 	Answer         string    `gorm:"size:255"`
 	CreatedAt      time.Time `gorm:"autoCreateTime"`
@@ -46,9 +46,9 @@ type Flashcard struct {
 type Flashcards []Flashcard
 
 type Test struct {
-	ID             int `gorm:"primaryKey"`
-	UserID         int `gorm:"index"`
-	SetID          int `gorm:"index"`
+	ID             int    `gorm:"primaryKey"`
+	UserGoogleID   string `gorm:"index"`
+	SetID          int    `gorm:"index"`
 	StartTime      time.Time
 	EndTime        time.Time
 	TotalQuestions int
