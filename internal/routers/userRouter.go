c537e1cf49a3802751664faeffc77142fac9a0f7
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-12-18 16:26:02 +0100

    auth/me to get user info

--- internal/routers/userRouter.go
+++ internal/routers/userRouter.go
@@ -16,6 +16,7 @@ func UserRouter(userHandler *handler.UserHandler) chi.Router {
 	r.Post("/", userHandler.Create)
 	r.Get("/", userHandler.List)
 	r.With(middlewares.CheckLoggedInMiddleware).Get("/", userHandler.List)
+	r.With(middlewares.UserIdFromSession).Get("/me", userHandler.GetByGoogleID)
 	r.Get("/{id}", userHandler.GetByID)
 	r.Put("/{id}", userHandler.UpdateByID)
 	r.Delete("/{id}", userHandler.DeleteByID)
