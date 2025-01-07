ad93938948666c63240aaa14816088d0e3c251a5
DawidBudzynsky <dawidbudzynski18@gmail.com>
2025-01-07 18:47:49 +0100

    added userFlaschardService functionality

--- internal/service/userFlashcardService.go
+++ internal/service/userFlashcardService.go
@@ -16,7 +16,7 @@ func NewUserFlashcardService(db *gorm.DB) *UserFlashcardService {
 
 func (s *UserFlashcardService) GetByCardID(cardID int) (*models.UserFlashcard, error) {
 	var userFlashcard models.UserFlashcard
-	err := s.db.First(&userFlashcard, "flashcardid = ?", cardID).Error
+	err := s.db.First(&userFlashcard, "flashcard_id = ?", cardID).Error
 	if err == nil {
 		return &userFlashcard, nil
 	}
@@ -26,4 +26,31 @@ func (s *UserFlashcardService) GetByCardID(cardID int) (*models.UserFlashcard, e
 	}
 
 	return nil, err
+}
+
+type CreateUserFlashcardRequest struct {
+	UserGoogleID              string `json:"google_id"`
+	FlashcardID               string `json:"username"`
+	Easiness                  string `json:"email"`
+	ConsecutiveCorrectAnswers string `json:"role"`
+}
+
+func (s *UserFlashcardService) Create(body *models.UserFlashcard) (*models.UserFlashcard, error) {
+	if err := s.db.Create(body).Error; err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (s *UserFlashcardService) UpdateByID(id int, body *models.UserFlashcard) (*models.UserFlashcard, error) {
+	userFlashcard, err := s.GetByCardID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.db.Model(&models.UserFlashcard{}).Where("flashcard_id = ?", id).Updates(body).Error; err != nil {
+		return nil, err
+	}
+	return userFlashcard, nil
 }
