57864610d9a0656b5a80c037c2edd5978c8dcbc4
DawidBudzynsky <dawidbudzynski18@gmail.com>
2025-01-13 20:34:44 +0100

    added breadcrumbs

--- flashcards/src/components/navbar.tsx
+++ flashcards/src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import handleLogout from '../requests/logout';
 import api from '../api/api';
+import Breadcrumbs from './Breadcrumbs';
 // import { Link } from 'react-router-dom';
 // import checkIfLoggedIn from '../requests/check_session';
 // import handleLogout from '../requests/logout';
@@ -74,8 +75,10 @@ const Navbar: React.FC = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
+
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
+
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     <li><a href='/'>Home</a></li>
@@ -93,6 +96,7 @@ const Navbar: React.FC = () => {
                     <li><a href='/profile'>Profile</a></li>
 
                 </ul>
+                <Breadcrumbs />
             </div>
             <div className="navbar-end">
                 <label className="swap swap-rotate">
@@ -116,6 +120,7 @@ const Navbar: React.FC = () => {
                             d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
                     </svg>
                 </label>
+
                 {!isLoggedIn ? (
                     <button onClick={handleLogin} className="btn">Sign In</button>
                 ) : (
